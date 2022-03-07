ccb0f51c1c96baa1c23395a408988428232ba5b9
Srujan <srujanravi30@gmail.com>
2022-03-07 22:56:37 +0530

    add info

--- resume_app.py
+++ resume_app.py
@@ -10,10 +10,10 @@ class resume_window:
 
         
 
-        Label(self.master,text="Srujan Kumar R",bd=11,relief=RAISED,bg="red",font=("times new roman",30,"bold"),pady=2).pack(fill=X)
+        Label(self.master,text="Name",bd=11,relief=RAISED,bg="red",font=("times new roman",30,"bold"),pady=2).pack(fill=X)
         
         
-        F0=Label(self.master,text="Email: srujanravi30@gmail.com",bd=5,font=("times new roman",30,"bold"),relief=RAISED,bg="yellow")
+        F0=Label(self.master,text="Email: email@email.com",bd=5,font=("times new roman",30,"bold"),relief=RAISED,bg="yellow")
         F0.place(x=0,y=73,height=60,relwidth=1)
         F1=LabelFrame(self.master,bd=10,relief=GROOVE,text="RESUME",font=("times new roman",15,"bold"),fg="black",bg="red")
         F1.place(x=0,y=133,relwidth=1,height=100)
@@ -54,9 +54,9 @@ class resume_window:
         self.txtarea.delete('1.0',END)
         self.welcome(string='CERTIFICATES')
 
-        my_img3 = ImageTk.PhotoImage((Image.open('coursera_python.jpg')).resize((1000,650),Image.ANTIALIAS))
-        my_img1 = ImageTk.PhotoImage((Image.open('sslc.jpg')).resize((500,650),Image.ANTIALIAS))
-        my_img2 = ImageTk.PhotoImage((Image.open('12th.jpg')).resize((500,650),Image.ANTIALIAS))
+        my_img3 = ImageTk.PhotoImage((Image.open('img1.jpg')).resize((1000,650),Image.ANTIALIAS))
+        my_img1 = ImageTk.PhotoImage((Image.open('img2.jpg')).resize((500,650),Image.ANTIALIAS))
+        my_img2 = ImageTk.PhotoImage((Image.open('img3.jpg')).resize((500,650),Image.ANTIALIAS))
 
         self.txtarea.image_create(END, image=my_img1)
         self.txtarea.image_create(END, image=my_img2)
@@ -67,9 +67,7 @@ class resume_window:
     def education(self):
         self.txtarea.delete('1.0',END)
         self.welcome(string='EDUCATION')
-        self.txtarea.insert(END ,'''•  Pursuing B.Tech in Information Science(ISE) at University Visvesvaraya college of Engineering. 
-•	12th from Mes KK Pu College. 
-•	S S L C from The New Cambridge High School
+        self.txtarea.insert(END ,'''•  add info
 ''')
  
     
@@ -82,11 +80,7 @@ class resume_window:
         
         self.txtarea.delete('1.0',END)
         self.welcome(string='PROGRAMMING LANGUAGES')
-        self.txtarea.insert(END,'''o	C
-o	C++
-o	HTML,CSS
-o	Python: Completed an online non-credit Specilization authorized by University of Michigan and offered through Coursera(coursera.org/verify/specialization/RZ8M44P94CY2). 
-''')
+        self.txtarea.insert(END,'''o    add info''')
        
         
         
@@ -98,16 +92,13 @@ o	Python: Completed an online non-credit Specilization authorized by University 
     def sw_tools(self):
         self.txtarea.delete('1.0',END)
         self.welcome(string='SOFTWARE TOOLS')
-        self.txtarea.insert(END,'''o	Google Cloud Console: Participated in Google Cloud Ready Program and was able to get hands on experience in the cloud console. 
-o	Robotic Process Automation: Have worked on Web scrapping and process automation using the UI Path tool. 
+        self.txtarea.insert(END,'''o	add info
 ''')
 
     def projects(self):            
         self.txtarea.delete('1.0',END)
         self.welcome(string='PROJECTS')
-        self.txtarea.insert(END,'''o	Website: Have created a website using the Flask framework and a HTML template and deployed it using Heroku.( https://github.com/YatishKV/SLE-website)
-o	Game: Have created an original spin off on the classic 2048 game using the pygame library and deployed it on windows using the pyinstaller package and deployed it on android using the buildozer tool on linux.(https://github.com/YatishKV/NoT2048game)
-''')
+        self.txtarea.insert(END,'''o	add info''')
 
 
 root=Tk()
